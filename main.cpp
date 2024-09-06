aa11f1c109824c5941b078af4fe441d82eff8cc5
sarah-naji <sarah.naji@upr.edu>
2024-09-06 19:02:51 +0000

    displayed purpose

--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
 // Sarah Naji sarah-naji 801-22-7451
+#include<iostream>
+#include <iomanip>
+using namespace std;
+
+int main (){ 
+    // Purpose of program
+    cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater. << endl;
