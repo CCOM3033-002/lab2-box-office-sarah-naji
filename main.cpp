7aa7b9d806ca7500b7d06630382fbb49751a9ca3
sarah-naji <sarah.naji@upr.edu>
2024-09-12 01:03:30 +0000

    constants

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // Sarah Naji sarah-naji 801-22-7451
 #include<iostream>
+#include <string>
 #include <iomanip>
 
 using namespace std;
@@ -7,4 +8,10 @@ using namespace std;
 int main (){ 
     // Purpose of program
     cout << "This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movie playing in the theater. << endl;
+
+
     // Variables constant
+    string movieName;
+    double adult_tickets, child_tickets;
+
+    
